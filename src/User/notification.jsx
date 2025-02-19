4df6c65e01736264569a19c31c4277798c1d1c71
daksh963655 <daksh.kumar@aayaninfotech.com>
2025-02-19 16:23:50 +0530

    change

--- src/User/notification.jsx
+++ src/User/notification.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LoggedHeader from "./auth/component/loggedNavbar";
+import LoggedHeader from "./Auth/component/loggedNavbar";
 import { Link } from "react-router-dom";
 import { MdMessage } from "react-icons/md";
 import "./user.css";
