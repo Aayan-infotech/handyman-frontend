9d8bdbb83d1c69beb58205d629b2d6b40e5339d7
daksh963655 <daksh.kumar@aayaninfotech.com>
2025-02-24 16:26:10 +0530

    Update firestore.jsx

--- src/Chat/lib/firestore.jsx
+++ src/Chat/lib/firestore.jsx
@@ -2,21 +2,21 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getDatabase } from "firebase/database"; 
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_V9Z2fXnrk-AeIqPjj0v8jFOspJ3EggE",
-  authDomain: "trade-hunter-4422d.firebaseapp.com",
-  projectId: "trade-hunter-4422d",
-  storageBucket: "trade-hunter-4422d.firebasestorage.app",
-  messagingSenderId: "476249897831",
-  appId: "1:476249897831:web:a8ad45ecfa4047ed774dc2",
-  measurementId: "G-G8N6W355CL",
+  apiKey: import.meta.env.VITE_FIRESTORE_APIKEY,
+  authDomain: import.meta.env.VITE_FIRESTORE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_STORAGE_MESSAGINGSENDID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASURNMENTID,
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app)
-export const db = getFirestore(app)
-export const storage = getStorage(app)
-export const realtimeDb = getDatabase(app)
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const realtimeDb = getDatabase(app);
