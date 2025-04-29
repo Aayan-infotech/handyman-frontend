ab755048d5a46eab41c1d2181c41e95bcdd52594
daksh963655 <daksh.kumar@aayaninfotech.com>
2025-04-29 17:54:40 +0530

    Update message.jsx

--- src/Chat/message.jsx
+++ src/Chat/message.jsx
@@ -198,7 +198,7 @@ export default function Message() {
       <div className="bg-second">
         <div className="container">
           {open && (
-            <FaArrowLeft onClick={() => setOpen(false)} className="mt-4 fs-4" />
+            <FaArrowLeft onClick={() => setOpen(false)} className="mt-4 fs-4" style={{ cursor: "pointer" }} />
           )}
           <div className="top-section-main py-4 px-lg-5">
             {!open && (
