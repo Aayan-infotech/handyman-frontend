2752bc335b56a97f2b75141f109f2d24dda41699
daksh963655 <daksh.kumar@aayaninfotech.com>
2025-07-02 16:45:45 +0530

    Update Home.jsx

--- src/Provider/Home.jsx
+++ src/Provider/Home.jsx
@@ -129,9 +129,6 @@ export default function HomeProvider() {
         handleAllData();
       }
     });
-
-
-    
     return () => {
       console.log("Cleaning up socket connection");
       newSocket.disconnect();
