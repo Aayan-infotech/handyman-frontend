4ae67ad7e0507de62992ee61886ef8f8f60d6fe3
daksh963655 <daksh.kumar@aayaninfotech.com>
2025-07-28 15:40:06 +0530

    Update notification.jsx

--- src/User/notification.jsx
+++ src/User/notification.jsx
@@ -252,7 +252,7 @@ export default function Notification() {
 
       await dispatch(
         assignedJobNotification({
-          body: `${hunterName} have been assigned for the job ${notification?.jobDetails?.title}`,
+          body: `You have been assigned for the job ${notification?.jobDetails?.title}`,
           receiverId: assignToId,
           jobId: notification.jobDetails._id,
         })
